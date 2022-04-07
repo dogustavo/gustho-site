310fe858a51c404ecb4009c0cab4e496e4008cad
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2022-04-07 20:15:29 -0300

    feat: adicionado font-size responsivo

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -12,8 +12,18 @@ export default createGlobalStyle`
     html,
     body {
         font-family: 'Josefin Sans', sans-serif;
-        font-size: 1em;
+        font-size: 4.44vw;
         scroll-behavior: smooth;
+
+        @media (min-width: 576px) {
+            font-size: 3vw;
+        }
+        @media (min-width: 768px) {
+            font-size: 2vw;
+        }
+        @media (min-width: 992px) {
+            font-size: 0.8333333333vw;
+        }
     }
     a, button {
         &:hover {
