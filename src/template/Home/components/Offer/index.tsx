726b4e43eb3fd2341b752d3340bc0cb00a122205
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2022-04-11 10:11:31 -0300

    feat(home): finalizado sessão O que oferecemos

--- src/template/Home/components/Offer/index.tsx
+++ src/template/Home/components/Offer/index.tsx
@@ -1,10 +1,53 @@
 import { Article } from 'components'
 
+import * as S from './styles'
+
+const bennefists = [
+  {
+    icon: '/static/img/free-delivery.svg',
+    title: 'Entrega Grátis',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.'
+  },
+  {
+    icon: '/static/img/cashback.svg',
+    title: 'Cashback',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.'
+  },
+  {
+    icon: '/static/img/premium-quality.svg',
+    title: 'Atendimento Premium',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.'
+  },
+  {
+    icon: '/static/img/support.svg',
+    title: 'Suport 24 horas',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.'
+  }
+]
+
 export default function FeaturedProducts() {
+  const renderBennefits = () => {
+    return bennefists.map((benefit, id) => (
+      <S.Card key={id}>
+        <img src={benefit.icon} alt={`Benefício de ${benefit.title}`} />
+
+        <S.CardDesc>
+          <S.CardTitle>{benefit.title}</S.CardTitle>
+          <S.Description>{benefit.description}</S.Description>
+        </S.CardDesc>
+      </S.Card>
+    ))
+  }
+
   return (
     <Article>
       <>
         <h2>O que oferecemos!</h2>
+        <S.Wrapper>{renderBennefits()}</S.Wrapper>
       </>
     </Article>
   )
--- src/template/Home/components/Offer/styles.tsx
+++ src/template/Home/components/Offer/styles.tsx
@@ -1,3 +1,62 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { breakpoints } from 'styles'
 
-export const Wrapper = styled.div``
+const sharedDisplay = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 17rem);
+  justify-content: center;
+  justify-items: center;
+  grid-gap: 1.5rem;
+
+  @media (max-width: ${breakpoints.small}) {
+    justify-content: flex-start;
+    grid-template-columns: repeat(4, 270px);
+    width: 100%;
+    padding: 20px;
+    overflow-x: scroll;
+
+    scroll-snap-type: x mandatory;
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+`
+
+export const Card = styled.div`
+  ${sharedDisplay}
+  flex-direction: column;
+  width: 100%;
+  padding: 3rem 2rem;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: -4px 6px 20px rgba(0, 0, 0, 0.1);
+`
+
+export const CardDesc = styled.div`
+  ${sharedDisplay}
+  flex-direction: column;
+  gap: 1.2rem;
+  margin-top: 1.2rem;
+`
+
+export const Description = styled.p`
+  color: ${({ theme }) => theme.colors.gray};
+  text-align: center;
+  line-height: 1.2rem;
+`
+export const CardTitle = styled.p`
+  font-family: 'Josefin Sans';
+  font-style: italic;
+  font-weight: 500;
+  font-size: 1.3rem;
+  line-height: 1.3rem;
+`
