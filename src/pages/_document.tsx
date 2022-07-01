113b6799f04667ba032a5c5e930377302b44c335
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2022-07-01 12:52:11 -0300

    fix: next font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,14 +37,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,700;1,400&family=Lato&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
